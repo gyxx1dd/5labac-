cb1b606a7d4b00271133387717d44009ee07a8b2
gyxx1dd <skiv08099@gmail.com>
2024-03-21 10:43:36 +0200

    11
    1

---
+++ Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3labac_
+{
+    public partial class Form1 : Form
+    {
+        WolfCollection wolfCollection;
+        private Wolf wolf;
+        private Wolf wolf1;
+        private Wolf wolf3;
+        private Wolf wolf4;
+
+        public Form1()
+        {
+            InitializeComponent();
+            wolfCollection = new WolfCollection();
+            wolf = new Wolf(30, 3, 50, "Grey Wolf", "Forests of North America");
+            wolf1 = new Wolf(10, 2, 50, "Grey Wolf", "Forests of North America");
+            wolf3 = new Wolf(1, 1, 1, "Grey Wolf", "Forests of North America");
+            wolf4 = new Wolf(2, 2, 2, "Grey Wolf", "Forests of North America");
+            wolfCollection.AddToArrayList(wolf);
+            wolfCollection.AddToArrayList(wolf1);
+            wolfCollection.AddToList(wolf3);
+            wolfCollection.AddToList(wolf4);
+
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Wolf Information:\n\n" +
+                            $"Weight: {wolf.Weight} kg\n" +
+                            $"Age: {wolf.Age} years\n" +
+                            $"Daily Maintenance Cost: {wolf.DailyMaintenanceCost}$\n" +
+                            $"Breed: {wolf.Breed}\n" +
+                            $"Natural Habitat: {wolf.NaturalHabitat}",
+                            "Wolf Info",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Information);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string wolfInfo = wolfCollection.DisplayArrayList();
+            MessageBox.Show(wolfInfo, "Wolf Collection", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string wolfInfo = wolfCollection.DisplayList();
+            MessageBox.Show(wolfInfo, "Wolf Collection", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _3labac_
+{
+    public interface IDisplayable
+    {
+        void DisplayInfo();
+    }
+
+    public interface IComparable<T>
+    {
+        int CompareTo(T other);
+    }
+
+    public interface ICloneable
+    {
+        object Clone();
+    }
+
+    public class Animal : IDisplayable, IComparable<Animal>, ICloneable
+    {
+        public double Weight { get; set; }
+        public int Age { get; set; }
+        public double DailyMaintenanceCost { get; set; }
+
+        public Animal(double weight, int age, double dailyMaintenanceCost)
+        {
+            Weight = weight;
+            Age = age;
+            DailyMaintenanceCost = dailyMaintenanceCost;
+        }
+
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine($"Weight: {Weight} kg");
+            Console.WriteLine($"Age: {Age} years");
+            Console.WriteLine($"Daily Maintenance Cost: {DailyMaintenanceCost}$");
+        }
+
+        public int CompareTo(Animal other)
+        {
+            if (other == null)
+                return 1;
+            return this.Weight.CompareTo(other.Weight);
+        }
+
+        public object Clone()
+        {
+            return new Animal(this.Weight, this.Age, this.DailyMaintenanceCost);
+        }
+    }
+
+    public class Wolf : Animal
+    {
+        public string Breed { get; set; }
+        public string NaturalHabitat { get; set; }
+
+        public Wolf(double weight, int age, double dailyMaintenanceCost, string breed, string naturalHabitat)
+            : base(weight, age, dailyMaintenanceCost)
+        {
+            Breed = breed;
+            NaturalHabitat = naturalHabitat;
+        }
+
+        public override void DisplayInfo()
+        {
+            base.DisplayInfo();
+            Console.WriteLine($"Breed: {Breed}");
+            Console.WriteLine($"Natural Habitat: {NaturalHabitat}");
+        }
+
+        public int CompareTo(Wolf other)
+        {
+            if (other == null)
+                return 1;
+            return this.Weight.CompareTo(other.Weight);
+        }
+
+        public new object Clone()
+        {
+            return new Wolf(this.Weight, this.Age, this.DailyMaintenanceCost, this.Breed, this.NaturalHabitat);
+        }
+    }
+    public  class WolfCollection
+    {
+        private ArrayList wolvesArrayList = new ArrayList();
+        private List<Wolf> wolvesList = new List<Wolf>();
+        public void AddToArrayList(Wolf wolf)
+        {
+            wolvesArrayList.Add(wolf);
+        }
+
+        public void AddToList(Wolf wolf)
+        {
+            wolvesList.Add(wolf);
+        }
+
+        public string DisplayArrayList()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (Wolf wolf in wolvesArrayList)
+            {
+                sb.AppendLine($"Wolf Information:\n" +
+                                $"Weight: {wolf.Weight} kg\n" +
+                                $"Age: {wolf.Age} years\n" +
+                                $"Daily Maintenance Cost: {wolf.DailyMaintenanceCost}$\n" +
+                                $"Breed: {wolf.Breed}\n" +
+                                $"Natural Habitat: {wolf.NaturalHabitat}\n");
+            }
+            return sb.ToString();
+        }
+
+        public string DisplayList()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (Wolf wolf in wolvesList)
+            {
+                sb.AppendLine($"Wolf Information:\n" +
+                                $"Weight: {wolf.Weight} kg\n" +
+                                $"Age: {wolf.Age} years\n" +
+                                $"Daily Maintenance Cost: {wolf.DailyMaintenanceCost}$\n" +
+                                $"Breed: {wolf.Breed}\n" +
+                                $"Natural Habitat: {wolf.NaturalHabitat}\n");
+            }
+            return sb.ToString();
+        }
+    }
+}
